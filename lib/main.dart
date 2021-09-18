99cb46e0acf532b9a748188d12a9e99033308c6f
Sorathan Saetang <sorathan@moveplus.co.th>
2021-09-18 12:47:13 +0700

    lock screen rotation with flutter/services

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,28 @@ import 'package:todoapp/modules/Todo/index.dart';
 import 'package:todoapp/modules/Home/index.dart';
 import 'package:todoapp/modules/Security/index.dart';
 import 'package:get/get.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFFFCAEB7),
-        brightness: Brightness.light,
-        primaryColor: const Color(0xFFFFFFFF),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp])
+      .then((view) {
+    runApp(
+      GetMaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: const Color(0xFFFCAEB7),
+          brightness: Brightness.light,
+          primaryColor: const Color(0xFFFFFFFF),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: '/home',
+        getPages: [
+          GetPage(name: '/home', page: () => const HomePage()),
+          GetPage(name: '/security', page: () => SecurityPage()),
+          GetPage(name: '/todo', page: () => TodoList())
+        ],
       ),
-      initialRoute: '/home',
-      getPages: [
-        GetPage(name: '/home', page: () => const HomePage()),
-        GetPage(name: '/security', page: () => SecurityPage()),
-        GetPage(name: '/todo', page: () => TodoList())
-      ],
-    ),
-  );
+    );
+  });
 }
