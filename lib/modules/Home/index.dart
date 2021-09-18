fa19f13d66c7a9e01612fcba419e2116fc7bc02e
Sorathan Saetang <sorathan@moveplus.co.th>
2021-09-18 10:38:09 +0700

    change home layout to center

--- lib/modules/Home/index.dart
+++ lib/modules/Home/index.dart
@@ -10,43 +10,45 @@ class HomePage extends GetView {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 150,
-                backgroundColor: Colors.white,
-                child: Image.asset('assets/logos/todologo.png'),
-              ),
-              const Padding(padding: EdgeInsets.all(40)),
-              const Text(
-                "WELCOME",
-                style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              const Text(
-                "Press Let's Start to Enjoy with Todo",
-                style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.white,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 150,
+                  backgroundColor: Colors.white,
+                  child: Image.asset('assets/logos/todologo.png'),
                 ),
-                textAlign: TextAlign.center,
-              ),
-              const Padding(padding: EdgeInsets.all(50)),
-              FloatingActionButton.extended(
-                onPressed: () {
-                  Get.toNamed('security');
-                },
-                label: const Icon(Icons.arrow_forward_ios),
-                icon: const Text("Let's Start"),
-                backgroundColor: Colors.pink,
-                elevation: 0,
-              ),
-            ],
+                const Padding(padding: EdgeInsets.all(40)),
+                const Text(
+                  "WELCOME",
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  "Press Let's Start to Enjoy with Todo",
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const Padding(padding: EdgeInsets.all(50)),
+                FloatingActionButton.extended(
+                  onPressed: () {
+                    Get.toNamed('security');
+                  },
+                  label: const Icon(Icons.arrow_forward_ios),
+                  icon: const Text("Let's Start"),
+                  backgroundColor: Colors.pink,
+                  elevation: 0,
+                ),
+              ],
+            ),
           ),
         ),
       ),
